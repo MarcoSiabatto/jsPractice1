79ab2b62d1d48fd9e3302c2a6cea45e04f701f3a
MarcoSiabatto <siabattorodriguezmarco@gmail.com>
2021-02-27 16:57:18 -0500

    Last update for js practice

--- practice15.js
+++ practice15.js
@@ -1,2 +1,14 @@
 // Mini Calculator
-let op = prompt("Please Select Operation: \n 1. Summation \n 2. Substraction \n 3. Multiplication \n 4. Division");
+const miniCalculator = () => {
+  let op = prompt(
+    "Please Select a Number to Execute Operation: \n 1. Summation \n 2. Substraction \n 3. Multiplication \n 4. Division"
+  );
+  // Validate the selected option
+  // parseInt(op) >0 && parseInt(op) <= 4
+  // op == "1" || op == "2" || op == "3" || op == "4"
+  if (op === "1" || op === "2" || op === "3" || op === "4") {
+    alert("You Must Choose an Option Between 1 and 4");
+  } else {
+    alert("Entered Else");
+  }
+};
